cb3a55c81486d083969b173cbe19ea39741cb8d6
Ash <penguinuniverse376@gmail.com>
2026-03-04 01:14:47 +0800

    feat: integrated openapi schema into functions

--- api/function_app.py
+++ api/function_app.py
@@ -1,25 +1,29 @@
 import azure.functions as func
-import logging
+from azure_functions_openapi.openapi import get_openapi_json, get_openapi_yaml
+from azure_functions_openapi.swagger_ui import render_swagger_ui
+
+from user import bp as user_blueprint
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app.register_blueprint(user_blueprint)
 
-@app.route(route="http_trigger_test")
-def http_trigger_test(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+@app.route(route="openapi.json", auth_level=func.AuthLevel.ANONYMOUS)
+def openapi_json(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        get_openapi_json(title="CowHorse API", version="1.0.0"),
+        mimetype="application/json",
+    )
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+
+@app.route(route="openapi.yaml", auth_level=func.AuthLevel.ANONYMOUS)
+def openapi_yaml(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        get_openapi_yaml(title="CowHorse API", version="1.0.0"),
+        mimetype="application/x-yaml",
+    )
+
+
+@app.route(route="docs", auth_level=func.AuthLevel.ANONYMOUS)
+def docs(req: func.HttpRequest) -> func.HttpResponse:
+    return render_swagger_ui(title="CowHorse API Docs", openapi_url="/api/openapi.json")

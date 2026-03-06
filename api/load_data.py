991897e0d02255d0bb412a9d996ee2c31e787506
Wallace-Tan <wallacetan1125@gmail.com>
2026-03-07 03:45:53 +0800

    feat: bulk load data to storage table

--- api/load_data.py
+++ api/load_data.py
@@ -1,12 +1,27 @@
-import json
 from azure.data.tables import TableClient
-import os
+import json, os
+import azure.functions as func
+from azure_functions_openapi.decorator import openapi
 
-conn_str = os.environ["AZURE_STORAGE_CONNECTION_STRING"]
-table = TableClient.from_connection_string(conn_str, "user")
+bp = func.Blueprint(name='user', url_prefix='/api/user')
 
-with open("dataset/user.json") as f:
-    entities = json.load(f)
+@bp.route(route="users/bulk", methods=["POST"])
+def bulk_insert_users(req: func.HttpRequest) -> func.HttpResponse:
 
-for e in entities:
-    table.upsert_entity(e)
+    entities = req.get_json()
+
+    table = TableClient.from_connection_string(
+        conn_str=os.environ["AZURE_STORAGE_CONNECTION_STRING"],
+        table_name="user"
+    )
+
+    table.create_table_if_not_exists()
+
+    for e in entities:
+        table.upsert_entity(entity=e)
+
+    return func.HttpResponse(
+        json.dumps({"message": "Data inserted successfully"}),
+        status_code=201,
+        mimetype="application/json"
+    )
